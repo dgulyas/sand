3d62a51940edd60d18e34bb6181b1aae0ec32434
David Gulyas <dgulyas@invidi.com>
2018-10-22 10:25:10 -0600

    Make impression move

--- Box/MainWindow.xaml.cs
+++ Box/MainWindow.xaml.cs
@@ -6,22 +6,25 @@ namespace Box
 {
 	public partial class MainWindow : Window
 	{
-		private const int Size = 23;
+		private const int Size = 100;
 		private readonly SandColumn[,] m_sand = SandTable.CreateSandTable(Size, Size, 15);
+		private readonly Point m_holeLocation;
 
 		public MainWindow()
 		{
 			InitializeComponent();
+			m_holeLocation = new Point {X = 6, Y = 6, Height = 10};
 		}
 
 		private void NextButton_OnClick(object sender, RoutedEventArgs e)
 		{
 			//((MainViewModel)this.DataContext).AddBox(new Sand.Point{X=0,Y=0}, 1,1,1);
 			((MainViewModel)DataContext).Clear();
-
+			m_holeLocation.X++;
+			m_holeLocation.Y++;
 
 			SandTable.ClearHeightLimits(m_sand);
-			SandTable.ApplyHeightLimitPattern(m_sand, HeigthLimitPatternLibrary.SmallCube, new Point { X = 10, Y = 10, Height = 10 });
+			SandTable.ApplyHeightLimitPattern(m_sand, HeigthLimitPatternLibrary.SmallCube, m_holeLocation);
 			SandTable.SettleMapTwoPass(m_sand);
 
 			foreach (var column in m_sand)
